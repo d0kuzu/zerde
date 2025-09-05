f6f3854c505ca572e8eac380e94a08522d28109d
d0kuzu <dkobdabaev@mail.ru>
2025-09-06 00:58:27 +0500

    upgraded get conversdation

--- api/infrastructure/controllers/chat_controllers/chat_controller.go
+++ api/infrastructure/controllers/chat_controllers/chat_controller.go
@@ -2,6 +2,7 @@ package chat_controllers
 
 import (
 	"AISale/config"
+	"AISale/services/airtable"
 	twilio "AISale/services/twillio"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -17,18 +18,18 @@ func NewChatHandler(cfg *config.Settings) *ChatHandler {
 }
 
 func (h *ChatHandler) GetAllChats(c *gin.Context) {
-	// get numbers from airtable
+	client := airtable.NewClient(h.cfg.ApiKey, h.cfg.BaseID)
 
-	//client := airtable.NewClient(h.cfg.ApiKey, h.cfg.BaseID)
-	//
-	//records, err := client.ListRecords(h.cfg.TableName)
-	//if err != nil {
-	//	c.JSON(500, gin.H{"error": err.Error()})
-	//	return
-	//}
+	records, err := client.ListRecords(h.cfg.TableName)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
 
-	// get messages from twillio
+	c.JSON(200, gin.H{"answer": records})
+}
 
+func (h *ChatHandler) AddChat(c *gin.Context) {
 	clientNumber := "+14086851938"
 	botNumber := "+16693420294"
 
@@ -42,6 +43,4 @@ func (h *ChatHandler) GetAllChats(c *gin.Context) {
 	for _, m := range messages {
 		fmt.Printf("[%s] %s → %s: %s\n", m.DateCreated, m.From, m.To, m.Body)
 	}
-
-	c.JSON(200, gin.H{"answer": ""})
 }
