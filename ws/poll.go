c4fbf13da98eef9407cc3ce8c57f3ed37bc4e837
d0kuzu <dkobdabaev@mail.ru>
2025-09-07 10:34:52 +0500

    fixed twillio polling

--- ws/poll.go
+++ ws/poll.go
@@ -2,9 +2,7 @@ package ws
 
 import (
 	twilio "AISale/services/twillio"
-	"fmt"
 	"log"
-	"sort"
 	"time"
 )
 
@@ -23,38 +21,34 @@ func PollTwilio(chatID, accountSID, authToken string) {
 		for _, m := range messages {
 			Broadcast(chatID, m.Body)
 			lastMessageSID = m.Sid
-			fmt.Printf(lastMessageSID)
 		}
 	}
 }
 
 func fetchMessagesFromTwilio(chatID, lastSID, accountSID, authToken string) ([]twilio.Message, error) {
 	botNumber := "+16693420294"
+
 	twilioClient := twilio.NewClient(accountSID, authToken)
 
-	allMessages, err := twilioClient.GetConversation(chatID, botNumber, 50)
+	messages, err := twilioClient.GetConversation(chatID, botNumber, 50)
 	if err != nil {
 		return nil, err
 	}
 
 	if lastSID == "" {
-		sort.Slice(allMessages, func(i, j int) bool {
-			return allMessages[i].DateCreated < allMessages[j].DateCreated
-		})
-		return allMessages, nil
+		return messages, nil
 	}
 
 	var result []twilio.Message
-	for _, msg := range allMessages {
-		if msg.Sid == lastSID {
-			break
+	found := false
+	for _, msg := range messages {
+		if found {
+			result = append(result, msg)
 		}
-		result = append(result, msg)
+		if msg.Sid == lastSID {
+			found = true
+		}
 	}
-
-	sort.Slice(result, func(i, j int) bool {
-		return result[i].DateCreated < result[j].DateCreated
-	})
 
 	return result, nil
 }
