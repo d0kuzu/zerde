6d09e33870e9e3389859f291f67560fc1623e809
d0kuzu <dkobdabaev@mail.ru>
2025-09-28 10:27:56 +0500

    hot fix

--- api/infrastructure/controllers/chat_controllers/chat_controller.go
+++ api/infrastructure/controllers/chat_controllers/chat_controller.go
@@ -63,11 +63,11 @@ func (h *ChatHandler) SearchChat(c *gin.Context) {
 
 	chat := c.Query("chat")
 
-	record, err := client.SearchChat(h.cfg.TableName, chat)
+	records, err := client.SearchChat(h.cfg.TableName, chat)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(200, gin.H{"answer": record})
+	c.JSON(200, gin.H{"answer": records})
 }
