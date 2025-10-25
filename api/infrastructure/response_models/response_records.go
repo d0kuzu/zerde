4312ac904732b88d701097fc4863c5ac47b281a6
d0kuzu <dkobdabaev@mail.ru>
2025-10-25 17:52:21 +0500

    response json fields names corrected

--- api/infrastructure/response_models/response_records.go
+++ api/infrastructure/response_models/response_records.go
@@ -1,10 +1,10 @@
 package response_models
 
 type ResponseRecord struct {
-	ID              string
-	MobileNumber    string
-	Status          string
-	ConversationID  string
-	MessagesCounter int
-	CreatedAt       string
+	ID              string `json:"id"`
+	MobileNumber    string `json:"mobileNumber"`
+	Status          string `json:"status"`
+	ConversationID  string `json:"conversationId"`
+	MessagesCounter int    `json:"messagesCounter"`
+	CreatedAt       string `json:"createdTime"`
 }
